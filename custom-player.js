9f9b64f9102a27352b81e81786092c027ecc2ac0
Pinoyradio <Europepinoyradio@gmail.com>
2024-07-23 21:17:25 +0200

    custom-player.js

---
+++ custom-player.js
@@ -0,0 +1,8 @@
+// custom-player.js
+// You can add additional functionality here if needed
+// For example, handling play/pause, volume control, etc.
+
+// Example: Log a message when the player starts
+console.log("Custom Icecast player initialized!");
+
+// You can add more custom logic as required
